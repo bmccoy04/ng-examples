b369a51f4ecebcc560edd394d187766c53df33d7
bmccoy04 <bryanmccoy04@gmail.com>
2019-04-22 21:22:35 -0500

    Added teh schedule page.

---
+++ ng-examples/src/app/schedule/schedule.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'schedule-root',
+  templateUrl: './schedule.component.html',
+  styleUrls: ['./schedule.component.css']
+})
+export class ScheduleComponent {
+  title = 'ng-examples';
+}
