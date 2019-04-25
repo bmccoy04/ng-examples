73a053c5c7a5741f835cfb7cd645db339b22bd0e
Bryan McCoy <bryanmccoy04@gmail.com>
2019-04-24 21:12:14 -0500

    Started adding managing items for schedules

--- ng-examples/src/app/app.module.ts
+++ ng-examples/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ScheduleComponent } from './schedule/schedule.component';
+import { RoomsComponent } from './schedule/room/rooms.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ScheduleComponent
+    ScheduleComponent,
+    RoomsComponent
   ],
   imports: [
     BrowserModule,
---
+++ ng-examples/src/app/schedule/room/rooms.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'rooms-root',
+    templateUrl: 'rooms.component.html'
+})
+
+export class RoomsComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
